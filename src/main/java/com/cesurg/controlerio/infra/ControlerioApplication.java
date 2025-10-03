c5ef08b126b8d31daac4c72645c5bd4085ce0e6d
Bruno de Oliveira Sartori <brunoo@MacBook-Air.local>
2025-10-03 16:43:51 -0300

    estruturação da arquitetura do projeto

---
+++ src/main/java/com/cesurg/controlerio/infra/ControlerioApplication.java
@@ -0,0 +1,13 @@
+package com.cesurg.controlerio.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlerioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlerioApplication.class, args);
+	}
+
+}
